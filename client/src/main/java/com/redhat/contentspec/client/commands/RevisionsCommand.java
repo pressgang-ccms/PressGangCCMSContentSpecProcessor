1fe11fb32d11bd1a90bbb60f9f6a0e2904880754
lnewson <lnewson@redhat.com>
2012-08-24 15:25:26 +1000

    Fixes to improve speeds when doing pull commands.

--- client/src/main/java/com/redhat/contentspec/client/commands/RevisionsCommand.java
+++ client/src/main/java/com/redhat/contentspec/client/commands/RevisionsCommand.java
@@ -141,7 +141,7 @@ public class RevisionsCommand extends BaseCommandImpl
 		}
 		else
 		{
-			revisions = restManager.getReader().getContentSpecRevisionsById(ids.get(0));
+			revisions = restManager.getReader().getContentSpecRevisionsById(ids.get(0), false);
 		}
 		
 		// Check that the content spec is valid
