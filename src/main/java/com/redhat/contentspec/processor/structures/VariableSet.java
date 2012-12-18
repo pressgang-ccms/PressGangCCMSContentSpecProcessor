56295a8ceaeac10d3de739c6bcfb09b89c2bbd4a
lnewson <lnewson@redhat.com>
2012-12-18 16:12:03 +1000

    Updates so far to use the abstraction layer.

--- src/main/java/com/redhat/contentspec/processor/structures/VariableSet.java
+++ src/main/java/com/redhat/contentspec/processor/structures/VariableSet.java
@@ -1,38 +1,31 @@
 package com.redhat.contentspec.processor.structures;
 
-public class VariableSet
-{
-	Integer startPos = null;
-	Integer endPos = null;
-	String contents = null;
-	
-	public Integer getStartPos()
-	{
-		return startPos;
-	}
-	
-	public void setStartPos(final Integer startPos)
-	{
-		this.startPos = startPos;
-	}
-	
-	public Integer getEndPos()
-	{
-		return endPos;
-	}
-	
-	public void setEndPos(final Integer endPos)
-	{
-		this.endPos = endPos;
-	}
-	
-	public String getContents()
-	{
-		return contents;
-	}
-	
-	public void setContents(final String contents)
-	{
-		this.contents = contents;
-	}
+public class VariableSet {
+    Integer startPos = null;
+    Integer endPos = null;
+    String contents = null;
+
+    public Integer getStartPos() {
+        return startPos;
+    }
+
+    public void setStartPos(final Integer startPos) {
+        this.startPos = startPos;
+    }
+
+    public Integer getEndPos() {
+        return endPos;
+    }
+
+    public void setEndPos(final Integer endPos) {
+        this.endPos = endPos;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(final String contents) {
+        this.contents = contents;
+    }
 }
