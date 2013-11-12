9d71456f061d7e0cd7cb37d971ea3a5e3ea39ac8
Lee Newson <lnewson@redhat.com>
2013-11-12 15:51:24 +1000

    Added BZ#1012250 - RFE: Support <index>es

--- src/main/java/org/jboss/pressgang/ccms/contentspec/processor/constants/ProcessorConstants.java
+++ src/main/java/org/jboss/pressgang/ccms/contentspec/processor/constants/ProcessorConstants.java
@@ -408,6 +408,6 @@ public class ProcessorConstants {
             CommonConstants.CS_MAVEN_ARTIFACT_ID_TITLE, CommonConstants.CS_FILE_TITLE, CommonConstants.CS_FILE_SHORT_TITLE,
             CommonConstants.CS_JIRA_PROJECT_TITLE, CommonConstants.CS_JIRA_COMPONENT_TITLE, CommonConstants.CS_JIRA_VERSION_TITLE,
             CommonConstants.CS_JIRA_LABELS_TITLE, CommonConstants.CS_JIRA_SERVER_TITLE, CommonConstants.CS_SPACES_TITLE,
-            CommonConstants.CS_ENTITIES_TITLE, CommonConstants.CS_DEFAULT_PUBLICAN_CFG_TITLE, CSConstants.DEBUG_TITLE,
-            CSConstants.OUTPUT_STYLE_TITLE, CSConstants.SURVEY_LINK_TITLE);
+            CommonConstants.CS_ENTITIES_TITLE, CommonConstants.CS_DEFAULT_PUBLICAN_CFG_TITLE, CommonConstants.CS_INDEX_TITLE,
+            CSConstants.DEBUG_TITLE, CSConstants.OUTPUT_STYLE_TITLE, CSConstants.SURVEY_LINK_TITLE);
 }
